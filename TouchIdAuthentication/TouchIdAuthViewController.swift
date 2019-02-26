0f6d3e076b45025f1846dcbe9676c128c5bb6cfe
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-02-26 12:23:48 +0800

    adding touch authentication

--- TouchIdAuthentication/TouchIdAuthViewController.swift
+++ TouchIdAuthentication/TouchIdAuthViewController.swift
@@ -15,7 +15,48 @@ class TouchIdAuthViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func authenticateUsingTouch(_ sender: Any) {
+        
+        // Creati a authentication context
+        let authenticationContext = LAContext()
+        var error:NSError?
+        
+        // Check if the device has a fingerprint sensor
+        // If not, show the user an alert view and bail out!
+        guard authenticationContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            
+            showAlertViewIfNoBiometricSensorHasBeenDetected()
+            return
+            
+        }
+        
+        // Checking  fingerprint
+        authenticationContext.evaluatePolicy(
+            .deviceOwnerAuthenticationWithBiometrics,
+            localizedReason: "Only awesome people are allowed",
+            reply: { [unowned self] (success, error) -> Void in
+                
+                if( success ) {
+                    
+                    // Fingerprint recognized
+                    // Go to view controller
+                    self.navigateToAuthenticatedViewController()
+                    
+                }else {
+                    
+                    // Check if there is an error
+                    if let error = error {
+                        
+                        let message = self.errorMessageForLAErrorCode(error.code)
+                        self.showAlertViewAfterEvaluatingPolicyWithMessage(message)
+                        
+                    }
+                    
+                }
+                
+        })
+    }
+    
 
 }
 
